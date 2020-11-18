777a78e5ba96ecf60805590b2e67eb2025442331
SoojungChae <naan_ace@naver.com>
2020-11-18 12:54:37 +0900

    Delete unusing files

--- components/molecules/input/CheckboxGroupInput.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CheckboxInput from './CheckboxInput';
-
-const CheckboxGroupInput = ({ data }) => {
-  return <CheckboxInput data={{ ...data, align: 'vertical' }} />;
-};
-
-export default CheckboxGroupInput;
--- components/molecules/input/RadioGroupInput.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RadioInput from './RadioInput';
-
-const RadioGroupInput = ({ data }) => {
-  return <RadioInput data={{ ...data, align: 'vertical' }} />;
-};
-
-export default RadioGroupInput;
